ca2e98f7d5775f05a00b16fb352e09c77d0eaf09
Gagan <gagandhyani17@gmail.com>
2026-04-16 10:41:33 +0530

    changes

--- app/about-us/About-us.tsx
+++ app/about-us/About-us.tsx
@@ -38,7 +38,7 @@ export default function AboutUs() {
         .rv-r.on { opacity:1; transform:none; }
 
         .banner-bg {
-          background-image: url('38.png');
+          background-image: url('abbt.png');
           background-size: cover;
           background-position: center 40%;
         }
