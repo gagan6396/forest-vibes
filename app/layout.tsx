acbd26cd84570f2942e64f03031a93e606fd3152
Gagan <gagandhyani17@gmail.com>
2026-05-12 13:21:28 +0530

    schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,29 @@ export const metadata: Metadata = {
   title: "Forrest Vibes | Luxury Nature Resort & Stay",
   description: " Looking for the best nature retreat? Forrest Vibes offers luxury cottages, organic dining, and scenic forest trails. Perfect for families, couples, and nature lovers.",
 };
-const schemaData = {
-  "@context": "https://schema.org",
-  "@type": "LodgingBusiness",
-  name: "Forrest Vibes",
-  url: "https://forrestvibes.com",
-  telephone: "+917500131319",
-  description: "Luxury nature resort with cottages, organic dining, and forest trails.",
-}
+const schemaData ={
+    "@context": "https://schema.org",
+    "@type": "Resort",
+    "name": "Forrest Vibes",
+    "image": "https://www.forrestvibes.com/logo.png",
+    "@id": "https://www.forrestvibes.com",
+    "url": "https://www.forrestvibes.com",
+    "telephone": "+917500131319",
+    "address": {
+      "@type": "PostalAddress",
+     "streetAddress": "Khasra No- 51, Moza, PO: Raipur, Bajhet",
+      "addressLocality": "Dehradun",
+      "addressRegion": "Uttarakhand",
+      "postalCode": "248008",
+      "addressCountry": "IN"
+    },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": 30.335520170064303,
+      "longitude": 78.10267869140513
+    },
+    "openingHours": "Mo-Su 06:00-21:00"
+  }
 export default function RootLayout({
   children,
 }: Readonly<{
